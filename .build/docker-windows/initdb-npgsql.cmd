e701d8d468b521431e6ad3edbc03f78cf0588517
Austin Drenski <austin.drenski@gmail.com>
2019-09-30 17:51:07 -0400

    Narrowed PostGIS issue to raster support
    Something is wrong/misconfigured in the PostGIS raster setup,
    making it impossible to add geospatial support through the modern
    'CREATE EXTENSION' mechanism.
    
    As a temporary workaround, add geospatial support manually via
    legacy scripts.

--- .build/docker-windows/initdb-npgsql.cmd
+++ .build/docker-windows/initdb-npgsql.cmd
@@ -8,3 +8,19 @@ psql -c "CREATE DATABASE npgsql_tests OWNER npgsql_tests;"
 : Domain account for Azure Pipelines.
 psql -c "CREATE ROLE vsts SUPERUSER LOGIN;"
 psql -c "CREATE DATABASE vsts OWNER vsts;"
+
+: TODO: Something is wrong with the raster setup (missing dep?)
+:       so we need to configure PostGIS manually.
+psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\postgis.sql
+psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\postgis_comments.sql
+psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\spatial_ref_sys.sql
+psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\topology.sql
+psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\topology_comments.sql
+
+: -- only if you compiled with raster (GDAL)
+: psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\rtpostgis.sql
+: psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\raster_comments.sql
+
+: --if you built with sfcgal support --
+: psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\sfcgal.sql
+: psql -d npgsql_tests -f C:\pgsql\share\contrib\postgis-2.5\sfcgal_comments.sql
